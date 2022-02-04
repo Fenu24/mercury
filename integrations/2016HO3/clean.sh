46beee8a589ddf870f37bff382068214a3e90a79
Marco Fenucci <marco_fenucci@matf.bg.ac.rs>
2022-02-04 12:30:53 +0100

    Removed integrations of 2016HO3 from the distribution

--- integrations/2016HO3/clean.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-rm -rf outputForward/*  2> /dev/null
-rm -rf outputBackward/*  2> /dev/null
-rm -rf kepOut/*.aei  2> /dev/null
-rm -rf closeOut/*  2> /dev/null
-rm -rd *.tmp 2> /dev/null
--- integrations/2016HO3_batch/clean.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-n_batch=20
-i=1
-while [ $i -le $n_batch ]
-do
-   rm -rf $i 2> /dev/null
-   i=$[$i+1]
-done
